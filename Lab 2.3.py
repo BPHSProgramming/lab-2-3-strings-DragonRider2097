82f3510ccbef6c2a6f33dda7891a8ab43d16bd83
DragonRider2097 <jlwats87@gmail.com>
2025-09-30 13:42:39 -0400

    Add files via upload

---
+++ Lab 2.3.py
@@ -0,0 +1,24 @@
+#Jordan Hosler-Watson, Period 8, 9/30/25
+print("Task 1")
+name = input("What is your first and last name ")
+print(len(name))
+space = name.find(" ")
+first_name = name[0:space]
+print(first_name)
+last_name = name[space:]
+print(last_name)
+
+
+
+
+
+print("Task 2")
+sw_phrase = "Once you start down the dark path, forever will it dominate your destiny."
+
+print("Task 3")
+
+print("Task 4")
+
+print("Task 5")
+
+print("Task 6")
